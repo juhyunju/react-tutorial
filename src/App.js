9e307041b0cfec8426aaa2ab30fcc10b27f306cf
juhyunju <juhyunju@naver.com>
2021-10-05 13:34:51 +0900

    컴포넌트화

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-function ArticleTag(){
+function Article(){
     return(
       <article>
       <h2>Welcome</h2>  
       Hello, WEB
     </article>  
     );
-  
+  }
+
+  function Nav(){
+    return (
+
+      <nav>
+      <ol>
+        <li><a href="1.html">html</a></li>
+        <li><a href="2.html">css</a></li>
+      </ol>
+    </nav>
+    );
   }
       
 
@@ -18,13 +29,8 @@ function App() {
   return (
     <div>
     <header><h1><a href="index.html">WEB</a></h1></header>
-    <nav>
-      <ol>
-        <li><a href="1.html">html</a></li>
-        <li><a href="2.html">css</a></li>
-      </ol>
-    </nav>
-      <ArticleTag></ArticleTag>
+      <Nav></Nav>
+      <Article></Article>
   </div>
   );
 }
